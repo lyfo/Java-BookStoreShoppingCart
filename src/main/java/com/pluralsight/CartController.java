f575671d70f7391b1d90772b6a80bf99ebf25605
lyfo <jefflyford@yahoo.com>
2019-05-19 21:59:10 -0700

    update

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -48,6 +48,9 @@ public class CartController extends HttpServlet {
 				case "/delete":
 					deleteFromCart(request, response);
 					break;
+				case "/update":
+					updateCart(request, response);
+					break;
         default:
            break;
 			}
@@ -67,6 +70,14 @@ public class CartController extends HttpServlet {
 		cart.deleteCartItem(index);
 	}
 
+	private void updateCart(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int index = Integer.parseInt(request.getParameter("index"));
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
+		cart.updateCartItem(index, quantity);
+	}
 
 
   protected void addToCart(HttpServletRequest request, HttpServletResponse response)
